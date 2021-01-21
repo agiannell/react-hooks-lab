0554abdb84839396efbe6da7e46063a721a0a2d6
agiannell <agiannell@gmail.com>
2021-01-21 15:08:15 -0700

    app 2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './Components/Character';
 import './App.css';
 
 function App() {
+  const [characterArr, setCharacterArr] = useState([]);
+
+  const getCharacters = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+      .then(res => {
+        setCharacterArr(res.data.results)
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  // console.log(characterArr);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      {characterArr.map((e, i) => (
+        <Character key={i} pokemon={e} />
+      ))}
     </div>
   );
 }
---
+++ app-2/src/Components/Character.js
@@ -0,0 +1,7 @@
+const Character = props => {
+    return (
+        <h2>{props.pokemon.name}</h2>
+    )
+}
+
+export default Character;
