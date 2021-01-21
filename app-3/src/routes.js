ae852a391f375137c5414a6f506e4933a379322c
agiannell <agiannell@gmail.com>
2021-01-21 16:30:45 -0700

    app 3 complete

---
+++ app-3/src/routes.js
@@ -0,0 +1,10 @@
+import { Switch, Route } from 'react-router-dom';
+import Pokemon from './Components/Pokemon';
+import PokemonList from './Components/PokemonList';
+
+export default (
+    <Switch>
+        <Route exact path='/' component={ PokemonList } />
+        <Route path='/pokemon/:name' component={ Pokemon } />
+    </Switch>
+)
